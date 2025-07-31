477f006f7884edcd8cb2f469dd1da3620089b04e
Tom Buclin <tom.bcln@eduge.ch>
2025-07-31 18:37:36 +0200

    Ajout du fichier index.php

---
+++ src/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/config/databaseconnect.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Page d'accueil</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>PAGE D'ACCUEIL</h1>
+        </div>
+
+            <!-- Barre de navigation -->
+            <div class="nav">
+                <a href="etudiants.php">Page Etudiants</a>
+                <a href="login.php">Login</a>
+                <a href="contact.php">Contact</a>
+            </div>
+
+    </body>
+</html>
+
+
+
