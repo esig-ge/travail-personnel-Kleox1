66597d7229362aa597b762c178de76f86aa98ed1
Tom Buclin <tom.bcln@eduge.ch>
2025-08-10 17:59:41 +0200

    Version finale du MPD

--- docs/MPD_BuclinTom.sql
+++ docs/MPD_BuclinTom.sql
@@ -1,39 +1,45 @@
 CREATE TABLE IF NOT EXISTS etudiant (
-	id_etudiant int AUTO_INCREMENT NOT NULL UNIQUE,
-	nom varchar(255) NOT NULL,
-	prenom varchar(255) NOT NULL,
-	classe varchar(255) NOT NULL,
-	date_naissance date NOT NULL,
-	statut BOOLEAN NOT NULL DEFAULT TRUE,
-	PRIMARY KEY (id_etudiant)
+    id_etudiant INT AUTO_INCREMENT NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    classe VARCHAR(55) NOT NULL, /* Réduction taille VARCHAR*/
+    date_naissance DATE NOT NULL,
+    statut BOOLEAN NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (id_etudiant)
 );
 
-CREATE TABLE IF NOT EXISTS note (
-	id_note int AUTO_INCREMENT NOT NULL UNIQUE,
-	periode date NOT NULL,
-	niveau_satisfaction varchar(255) NOT NULL,
-	id_etudiant int NOT NULL,
-	id_matiere int NOT NULL,
-	PRIMARY KEY (id_note),
-	FOREIGN KEY (id_etudiant) REFERENCES etudiant(id_etudiant),
-	FOREIGN KEY (id_matiere) REFERENCES matiere(id_matiere)
+CREATE TABLE IF NOT EXISTS utilisateur (
+    id_utilisateur INT AUTO_INCREMENT NOT NULL,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id_utilisateur)
+    
+);
+
+CREATE TABLE IF NOT EXISTS matiere (
+    id_matiere INT AUTO_INCREMENT NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id_matiere)
 );
 
 CREATE TABLE IF NOT EXISTS commentaire (
-	id_commentaire int AUTO_INCREMENT NOT NULL UNIQUE,
-	contenu TEXT NOT NULL,
-	date DATE NOT NULL,
-	type_comportement varchar(255) NOT NULL,
-	id_etudiant int NOT NULL,
-	PRIMARY KEY (id_commentaire),
-	FOREIGN KEY (id_etudiant) REFERENCES etudiant(id_etudiant)
+    id_commentaire INT AUTO_INCREMENT NOT NULL,
+    contenu TEXT NOT NULL,
+    date DATE NOT NULL,
+    type_comportement VARCHAR(55) NOT NULL, /* Réduction taille VARCHAR*/
+    id_etudiant INT NOT NULL,
+    PRIMARY KEY (id_commentaire),
+    FOREIGN KEY (id_etudiant) REFERENCES etudiant(id_etudiant)
 );
 
-CREATE TABLE matiere (
-	id_matiere int AUTO_INCREMENT NOT NULL UNIQUE,
-	nom varchar(255) NOT NULL,
-	PRIMARY KEY (id_matiere)
+/* Table d'association */
+CREATE TABLE IF NOT EXISTS note (
+    id_note INT AUTO_INCREMENT NOT NULL,
+    periode DATE NOT NULL,
+    niveau_satisfaction VARCHAR(255) NOT NULL,
+    id_etudiant INT NOT NULL,
+    id_matiere INT NOT NULL,
+    PRIMARY KEY (id_note),
+    FOREIGN KEY (id_etudiant) REFERENCES etudiant(id_etudiant),
+    FOREIGN KEY (id_matiere) REFERENCES matiere(id_matiere)
 );
-
-
-
