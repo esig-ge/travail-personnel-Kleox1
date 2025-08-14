241ae872f3aede7a0e515428b8127e2440c578e0
Tom Buclin <tom.bcln@eduge.ch>
2025-08-14 18:25:25 +0200

    Version finale du fichier index.php avec header, liens de navigations, l'image de fond et le footer

--- src/index.php
+++ src/index.php
@@ -1,12 +1,14 @@
 <?php
 
+// Connexion à la base de données et gestion de session
 require_once __DIR__ . '/config/databaseconnect.php';
+require_once __DIR__ . '/includes/session.php';
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="fr">
+
     <head>
         <meta charset="UTF-8">
         <title>Page d'accueil</title>
@@ -14,18 +16,39 @@ require_once __DIR__ . '/config/databaseconnect.php';
     </head>
 
     <body>
-        <div class="container">
-            <h1>PAGE D'ACCUEIL</h1>
-        </div>
+        <header>
+
+            <div class="container">
+                <h1>Accueil</h1>
+            </div>
 
             <!-- Barre de navigation -->
             <div class="nav">
                 <a href="etudiants.php">Page Etudiants</a>
-                <a href="login.php">Login</a>
-                <a href="contact.php">Contact</a>
+
+                <?php 
+                    if (isset($_SESSION['utilisateur_id'])){
+                        echo '<a href="logout.php">Déconnexion</a>';
+                    } else {
+                        echo '<a href="login.php">Login</a>';
+                    } 
+                ?>
+
             </div>
+        </header>
+
+        <!-- Image de fond -->
+        <main class="imageBackground">
+                <!-- Pas de contenu -->
+        </main>
+
+    <?php 
+        // Pied de page 
+        require_once __DIR__ . '/includes/footer.php'; 
+    ?>
 
     </body>
+    
 </html>
 
 
