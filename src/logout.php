baa452fa509f35639826d6716dbc32f2f6b4022d
Tom Buclin <tom.bcln@eduge.ch>
2025-08-14 18:30:18 +0200

    Version finale 2.0 de login.php avec gestion de session et vérification sécurisée du mot de passe

--- src/logout.php
+++ src/logout.php
@@ -1,9 +1,12 @@
 <?php
 
+// Démarrage de la session
 session_start();
 
+// Destruction de toutes les données de session pour déconnexion
 session_destroy();
 
+// Redirection vers la page de login
 header('location: login.php');
 exit; 
 
