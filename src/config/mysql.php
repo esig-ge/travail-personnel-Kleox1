0517bc4eebd38ab81438138ed9801c00acaf4b8c
Kleox1 <168177515+Kleox1@users.noreply.github.com>
2025-08-15 19:24:13 +0200

    Mise à jour pour infomaniak - mysql.php

--- src/config/mysql.php
+++ src/config/mysql.php
@@ -1,11 +1,11 @@
 <?php
 
-// Constantes pour se connecter à la base de données
+// Constantes pour se connecter à la base de données (Infomaniak)
 
-const MYSQL_HOST = 'localhost';
+const MYSQL_HOST = 'hhva.myd.infomaniak.com';
 const MYSQL_PORT = '3306';
-const MYSQL_NAME = 'suivi_etudiants'; 
-const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = '';
+const MYSQL_NAME = 'hhva_tom'; 
+const MYSQL_USER = 'hhva_tom';
+const MYSQL_PASSWORD = 'o8$25q3d!?_ZFKF';
 
 ?>
